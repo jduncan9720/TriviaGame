88e12b443a11d0cd343eed381256197e305458a4
Joshua Duncan <jduncan97@gmail.com>
2020-03-24 16:28:47 -0600

    more work on the JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
 //Create an array of question objects that hold your trivia questions 
+$("#gameHolder").hide();
+
 var questions = [{
     "question": "What does the 'J' stand for in David J. Matthews ?",
     "option1": "Jeff",
@@ -7,7 +9,7 @@ var questions = [{
     "option4": "Jack",
     "answer": "option2"  
 }, {
-    "question:": "What is the name of Dave Matthews Band's drummer ?",
+    "question": "What is the name of Dave Matthews Band's drummer ?",
     "option1": "Carter Beauford",
     "option2": "Carter Banks",
     "option3": "Banks Carter",
@@ -45,17 +47,58 @@ function startGame() {
     //show gameHolder div on click
     $("#gameHolder").show();
     //hide startDiv div on click
-}   $("#startdiv").hide();
-
-
-//This function will replace display whatever image it's given in in the 'src' attribute of the image tag
-function displayImage() {
-    $("#image-holder").html("<img src=" + questions[count] + "width = '400px'>");
+    $("#startDiv").hide();
+    nextQuestion();
 }
+
 function nextQuestion() {
+    $("#question").html((questions[count])["question"]);
+    $("#answer1").html((questions[count])["option1"]);
+    $("#answer2").html((questions[count])["option2"]);
+    $("#answer3").html((questions[count])["option3"]);
+    $("#answer4").html((questions[count])["option4"]);
+    count++;
+
 //Replace the question in the question-holder div with the question in the array at position count
 //Replace the button text with new answers
 }
+//$("#test").click(function () {nextQuestion()});**********************
+//Check if correct option is chosen then call a function correct() or function incorrect()
+$("#answer1").on("click", function () { buttonClick("option1") });
+$("#answer2").on("click", function () { buttonClick("option2") });
+$("#answer3").on("click", function () { buttonClick("option3") });
+$("#answer4").on("click", function () { buttonClick("option4") });
+
+function buttonClick(color) {
+    if (color == "blue") {
+        currentScore = crystal1 + currentScore;
+        $("#score").html(currentScore);
+    }
+    else if (color == "green") {
+        currentScore = crystal2 + currentScore;
+        $("#score").html(currentScore);
+    }
+    else if (color == "red") {
+        currentScore = crystal3 + currentScore;
+        $("#score").html(currentScore);
+    }
+    else if (color == "yellow") {
+        currentScore = crystal4 + currentScore;
+        $("#score").html(currentScore);
+    }
+//function correct()
+    //show "That is correct!"
+    //show image for answer
+    //timer here
+    //call function nextQuestion()
+
+//function incorrect()
+//show "That is incorrect"
+    //show image for answer
+    //timer here
+    //call function nextQuestion()
+
+
 function buttonClick(buttonAttribute){
 //Check the element at count, compare it's attribute with buttonAttribute, if they are the same, it's correct.
 //nameOfTopArray[1].element("question")
@@ -65,21 +108,4 @@ function buttonClick(buttonAttribute){
 }
 //Use showQuestion to hold the setInterval to run nextQuestion
 
-
-
-
-
-
-
-
-
-
-
-
-
-Message Drew Congino
-
-
-About this conversation
-
-
+}
